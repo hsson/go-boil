588ee4ea44423ebbe58f4555efef6b977905a1dd
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-07-25 15:34:25 +0200

    Adds support for getting static files

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+  "net/http"
   "github.com/urfave/negroni"
 )
 
 func main() {
 	r := NewRouter()
-	n := negroni.Classic()
-	n.UseHandler(r)
+	n := negroni.New()
+
+  // Recovery handling middleware
+  n.Use(negroni.NewRecovery())
+
+  // Logging middleware
+  n.Use(negroni.NewLogger())
+
+  // Static file serving middleware
+  n.Use(negroni.NewStatic(http.Dir("public")))
+
+  n.UseHandler(r)
 	n.Run(":8080")
 }
